04392279112ef4aa3978536ed6b1b491ebdc5b7f
jerryosorio-corestory <jerry.osorio@corestory.ai>
2026-01-13 22:04:33 -0600

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,35 @@
+public class Calculator {
+
+    // Addition
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    // Subtraction
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    // Multiplication
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    // Division
+    public double divide(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed");
+        }
+        return a / b;
+    }
+
+    // Main method for testing
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        System.out.println("Addition: " + calc.add(10, 5));
+        System.out.println("Subtraction: " + calc.subtract(10, 5));
+        System.out.println("Multiplication: " + calc.multiply(10, 5));
+        System.out.println("Division: " + calc.divide(10, 5));
+    }
+}
